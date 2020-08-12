7ab349b7755bfa344b6b9e7cf353fe254eeca6ea
cisabane7 <chaste.isabane@gmail.com>
2020-08-12 19:29:41 +0200

    Refactoring for Persistance of Status Change

---
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/AllocationFailureAction.java
@@ -0,0 +1,35 @@
+package guru.sfg.beer.order.service.sm.actions;
+
+import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
+import guru.sfg.brewery.model.events.AllocationFailureEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AllocationFailureAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+    private final JmsTemplate jmsTemplate;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> stateContext) {
+        String beerOrderId = stateContext.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER).toString();
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATION_FAILURE_QUEUE,
+                 AllocationFailureEvent.builder().orderId(UUID.fromString(beerOrderId))
+                .build());
+
+        log.debug("Send Allocation failure message to queue for Order Id : {}", beerOrderId);
+
+    }
+}
