143b0169eae4c1ef25603f2d4f502741db78b79d
cisabane7 <chaste.isabane@gmail.com>
2020-09-10 11:02:29 +0200

    Create beer service host property

--- src/main/java/guru/sfg/beer/order/service/config/BeerServiceProperties.java
+++ src/main/java/guru/sfg/beer/order/service/config/BeerServiceProperties.java
@@ -1,12 +1,12 @@
 package guru.sfg.beer.order.service.config;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
 public class BeerServiceProperties {
+    @Value("${sfg.brewery.beerServiceHost}")
     private String beerServiceHost;
 }
